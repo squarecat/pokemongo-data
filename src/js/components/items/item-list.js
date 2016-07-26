ea35e80e0fe144643b8c21044d1a45c00ad8e7d2
Danielle Johnson <dinkydani@googlemail.com>
2016-07-26 14:25:27 +0100

    Updated class to BEM

--- src/js/components/items/item-list.js
+++ src/js/components/items/item-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from './item';
 
 export default (props = { items: [] }) => (
-  <ul className="item-list">
+  <ul className="items__list">
     {
       props.items.map(i => (
         <Item key={ i.id } item={ i } />
